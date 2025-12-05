55347fbb90b507167779f67f0a138fc6e2d67ae8
xiaobai1226 <felix_pai@foxmail.com>
2025-12-05 16:54:20 +0800

    feat: 添加 ElIcon 组件类型声明
    - 在 Vue 的类型声明中新增 ElIcon 组件的类型定义，以支持 Element Plus 的图标组件。

--- ui/components.d.ts
+++ ui/components.d.ts
@@ -21,6 +21,7 @@ declare module 'vue' {
     ElDropdownMenu: typeof import('element-plus/es')['ElDropdownMenu']
     ElForm: typeof import('element-plus/es')['ElForm']
     ElFormItem: typeof import('element-plus/es')['ElFormItem']
+    ElIcon: typeof import('element-plus/es')['ElIcon']
     ElImage: typeof import('element-plus/es')['ElImage']
     ElImageViewer: typeof import('element-plus/es')['ElImageViewer']
     ElInput: typeof import('element-plus/es')['ElInput']
